479362fc3a3540a65c7dbfb928f29f881ec1d840
Wisula <99121477+Wisula@users.noreply.github.com>
2022-03-30 09:06:28 +0530

    Update main .py.py

--- main .py.py
+++ main .py.py
@@ -19,7 +19,7 @@ async def start(client, message):
                chat_id=message.chat.id,
                text="""<b>Hey There, I'm Telegraph Bot
 
-I can upload photos or videos to telegraph. Made by @slbotzone 🇱🇰
+I can upload photos or videos to telegraph. Made by @mwtgbots 🇱🇰
 
 Hit help button to find out more about how to use me</b>""",   
                             reply_markup=InlineKeyboardMarkup(
@@ -27,10 +27,10 @@ Hit help button to find out more about how to use me</b>""",
                                         InlineKeyboardButton(
                                             "Help", callback_data="help"),
                                         InlineKeyboardButton(
-                                            "Channel", url="https://t.me/sl_bot_zone")
+                                            "Channel", url="https://t.me/mwtgbots")
                                     ],[
                                       InlineKeyboardButton(
-                                            "Source Code", url="https://github.com/youtubeslgeekshow/Telegraphbot")
+                                            "Source Code", url="https://github.com/wisulaTelegraphbot")
                                     ]]
                             ),        
             disable_web_page_preview=True,        
@@ -43,9 +43,9 @@ async def help(client, message):
                chat_id=message.chat.id,
                text="""<b>Telegraph Bot Help!
 
-Just send a photo or video less than 5mb file size, I'll upload it to telegraph.
+Just send a photo or video less than 5mb file size, I'll upload it to telegraph. created by wisula
 
-~ @slbotzone</b>""",
+~ @mwtgbots</b>""",
         reply_markup=InlineKeyboardMarkup(
                                 [[
                                         InlineKeyboardButton(
@@ -67,9 +67,9 @@ async def about(client, message):
                chat_id=message.chat.id,
                text="""<b>About Telegraph Bot!</b>
 
-<b> Developer:</b> <a href="https://t.me/supunma">supunma 🇱🇰</a>
+<b> Developer:</b> <a href="https://t.me/wisula">wisula ✌️🇰</a>
 
-<b> bot updates :</b> <a href="https://t.me/sl_bot_zone"> bot updates</a>
+<b> bot updates :</b> <a href="https://t.me/mwtgbots"> bot updates</a>
 
 <b> Library:</b> <a href="https://github.com/pyrogram/pyrogram">Pyrogram</a>
 
@@ -79,7 +79,7 @@ async def about(client, message):
                                         InlineKeyboardButton(
                                             "Back", callback_data="help"),
                                         InlineKeyboardButton(
-                                            "Source Code", url="https://github.com/youtubeslgeekshow/Telegraphbot")
+                                            "Source Code", url="https://github.com/wisula/Telegraphbot")
                                     ]]
                             ),        
             disable_web_page_preview=True,        
@@ -95,7 +95,7 @@ async def telegraphphoto(client, message):
     except:
         await msg.edit_text("Photo size should be less than 5mb!") 
     else:
-        await msg.edit_text(f'**Uploaded To Telegraph!\n\n👉 https://telegra.ph{response[0]}\n\nJoin @slbotzone**',
+        await msg.edit_text(f'**Uploaded To Telegraph!\n\n👉 https://telegra.ph{response[0]}\n\nJoin @mwtgbots**',
             disable_web_page_preview=True,
         )
     finally:
@@ -111,7 +111,7 @@ async def telegraphvid(client, message):
     except:
         await msg.edit_text("Video size should be less than 5mb!") 
     else:
-        await msg.edit_text(f'**Uploaded To Telegraph!\n\n👉 https://telegra.ph{response[0]}\n\nJoin @slbotzone**',
+        await msg.edit_text(f'**Uploaded To Telegraph!\n\n👉 https://telegra.ph{response[0]}\n\nJoin @mwtgbots**',
             disable_web_page_preview=True,
         )
     finally:
@@ -127,7 +127,7 @@ async def telegraphgif(client, message):
     except:
         await msg.edit_text("Gif size should be less than 5mb!") 
     else:
-        await msg.edit_text(f'**Uploaded To Telegraph!\n\n👉 https://telegra.ph{response[0]}\n\nJoin @slbotzone**',
+        await msg.edit_text(f'**Uploaded To Telegraph!\n\n👉 https://telegra.ph{response[0]}\n\nJoin @tgbots**',
             disable_web_page_preview=True,
         )
     finally:
@@ -149,7 +149,7 @@ async def button(bot, update):
 print(
     """
 Bot Started!
-Join @slbotzone
+Join @mwtgbots
 """
 )
 
